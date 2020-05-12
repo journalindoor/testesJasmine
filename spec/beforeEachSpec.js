1a41fcc51e92b8f49af4c4cc99d5bace080dbbb7
RSMJr <journal.indoor@gmail.com>
2020-05-12 14:57:34 -0300

    [39] Execução de códigos - beforeEach

---
+++ spec/beforeEachSpec.js
@@ -0,0 +1,17 @@
+describe("Teste do beforeEach",  () => {
+
+    var contador = 0;
+
+    beforeEach( () => {
+        contador++;
+    });
+
+    it("Deve incrementar o contador para 1",  () => {
+        expect(contador).toEqual(1);
+    });
+
+    it("Deve incrementar o contador para 2",  () => {
+        expect(contador).toEqual(2);
+    });
+
+});
