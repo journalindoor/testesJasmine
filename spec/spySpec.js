ccf84674fa414795950bba24cd7fcafa3ce27e97
RSMJr <journal.indoor@gmail.com>
2020-05-13 15:58:40 -0300

    [54] Spies - and.callFake

--- spec/spySpec.js
+++ spec/spySpec.js
@@ -10,13 +10,8 @@ describe("Testes do objeto Spy", () => {
     };
 
     beforeAll(()=>{
-        spyOn(Calculadora, "somar").and.callThrough();
-        spyOn(Calculadora, "subtrair");
-    });
-    
-    it('Deve executar o método somar original', () => {
-        expect(Calculadora.somar(1,1)).toEqual(2);
-        expect(Calculadora.subtrair(2,1)).toBeUndefined();
+        spyOn(Calculadora, "somar").and.callFake();
+       
     });
 
 });
