bc279e23d88b0130e8d8a06fc9eec263004bc8df
RSMJr <journal.indoor@gmail.com>
2020-05-14 16:10:27 -0300

    [91] Configurando Karma com o Browserify

--- spec/exemplo/exemploSpec.js
+++ spec/exemplo/exemploSpec.js
@@ -2,4 +2,8 @@ describe("Suite de teste de exemplo com NodeJs", () => {
     it('Deve mostrar o funcionamento do Jasmine com o NodeJs', () => {
         expect(true).toEqual(true);
     });
+
+    it('Deve demonstrar o uso do Karma em modo de escuta', () => {
+        expect(1).toEqual(1);
+    });
 });
